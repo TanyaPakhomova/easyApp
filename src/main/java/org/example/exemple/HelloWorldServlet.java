839054b63216091ba20071a27e2140e4057996b1
Tanya Pakhomova <user@localhost>
2024-03-17 20:33:47 +0300

    init

---
+++ src/main/java/org/example/exemple/HelloWorldServlet.java
@@ -0,0 +1,23 @@
+package org.example.exemple;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(name = "HelloWorldServlet", urlPatterns = {"/hello"})
+
+public class HelloWorldServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Hello World Servlet</title></head>");
+        out.println("<body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
